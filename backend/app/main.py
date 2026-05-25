98075c5575c6bee42846a295e127119cd9377e7f
kirillbelykh <kirillbelykh228@gmail.com>
2026-05-25 13:14:51 +0300

    Add query parameter example endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,7 +7,14 @@ app = FastAPI()
 def index():
     return {"status": "backend is running!"}
 
+
+@app.get("/lessons")
+def get_lessons(limit: int = 10):
+    return {"lessons_limit": limit}
+
+
 @app.get("/lessons/{lesson_id}")
 def get_lesson_by_id(lesson_id: int):
-        
     return {"number": lesson_id}
+
+
