c7d4de4350b285c570a11da95a926b3955183c13
kirillbelykh <kirillbelykh228@gmail.com>
2026-05-25 15:17:33 +0300

    Add note schemas

---
+++ backend/app/schemas/note.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from pydantic import BaseModel
+
+
+class NoteCreate(BaseModel):
+    title: str
+    content: str
+    content_type: str 
+
+
+class NoteUpdate(BaseModel):
+    title: str | None = None
+    content: str | None = None
+    content_type: str | None = None
+
+
+class NoteResponse(BaseModel):
+    id: int
+    title: str
+    content: str
+    content_type: str
+    created_at: datetime 
+    updated_at: datetime
+    is_deleted: bool
