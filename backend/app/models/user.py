bd1e49449c5c5ee5c7894df2339496a76161f71a
kirillbelykh <kirillbelykh228@gmail.com>
2026-05-26 23:49:24 +0300

    Add User ORM model

---
+++ backend/app/models/user.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+from sqlalchemy import String, func
+from sqlalchemy.orm import Mapped,  mapped_column
+
+from backend.app.core.database import Base 
+
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
+    password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now(), nullable=False)
+    
