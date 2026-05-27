3f473c3ea994be1c4f2657f0f97d96d035424731
kirillbelykh <kirillbelykh228@gmail.com>
2026-05-27 14:37:21 +0300

    Add SQLAlchemy engine configuration

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,4 +1,9 @@
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import create_engine
+from backend.app.core.config import settings
+
+
+engine = create_engine(settings.database_url)
 
 class Base(DeclarativeBase):
     pass
