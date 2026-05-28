0a598a1f02a75dec06a9667ce298a4d89e42ce0b
kirillbelykh <kirillbelykh228@gmail.com>
2026-05-28 16:15:12 +0300

    Connect note creation endpoint to database

--- backend/app/api/notes.py
+++ backend/app/api/notes.py
@@ -1,20 +1,26 @@
-from datetime import datetime
-from fastapi import APIRouter, status
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
+
+from backend.app.dependencies.database import get_db
 from backend.app.schemas.note import NoteCreate, NoteResponse
+from backend.app.services.note import create_new_note
 
 
 router = APIRouter(prefix="/notes", tags=["notes"])
 
 
 @router.post("", response_model=NoteResponse, status_code=status.HTTP_201_CREATED)
-def create_note(request: NoteCreate):
-    now = datetime.now()
-    return {
-        "title": request.title,
-        "content": request.content,
-        "content_type": request.content_type,
-        "id": 1,
-        "created_at": now,
-        "updated_at": now,
-        "is_deleted": False
-    }
+def create_note(
+    request: NoteCreate,
+    db: Annotated[Session, Depends(get_db)],
+):
+    note = create_new_note(
+        db,
+        request,
+        user_id=3,
+    )
+    
+    return note
+    
