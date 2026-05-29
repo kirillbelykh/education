35efc456d66ecedf2bb0da44f264ab5c1b9a81aa
kirillbelykh <kirillbelykh228@gmail.com>
2026-05-29 10:52:27 +0300

    Handle failed note creation in service

--- backend/app/services/note.py
+++ backend/app/services/note.py
@@ -1,3 +1,5 @@
+from fastapi import HTTPException, status
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
 from backend.app.schemas.note import NoteCreate
@@ -9,11 +11,17 @@ def create_new_note(
     note_data: NoteCreate,
     user_id: int,
 ):
-    note = create_note(
-        db,
-        note_data,
-        user_id,
-    )
-    
+    try:
+        note = create_note(
+            db,
+            note_data,
+            user_id,
+        )
+    except IntegrityError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Unable to create note",
+        )
+        
     return note
 
