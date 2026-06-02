6b9af50e44829d88f28a7465439308173b83a937
kirillbelykh <kirillbelykh228@gmail.com>
2026-06-02 11:36:42 +0300

    Add user schemas

---
+++ backend/app/schemas/user.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from pydantic import BaseModel
+
+
+class UserCreate(BaseModel):
+    email: str
+    password: str
+    
+    
+class UserResponse(BaseModel):
+    id: int
+    email: str
+    created_at: datetime
